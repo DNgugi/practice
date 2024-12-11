1029e60b90dd357d0d6d82554024fce8609e30c8
DNgugi <dngugi42@gmail.com>
2024-12-11 08:27:16 +0300

    added passing tests for set of 7 numeral symbols, and single digit inputs

--- roman-numeral-converter/convertToRoman.js
+++ roman-numeral-converter/convertToRoman.js
@@ -2,6 +2,8 @@ const convertToRoman = num => {
     if (num === "") {
         return "invalid"
     }
+    let numLength = num.length;
+    // console.log(numLength)
     num = parseInt(num)
     if (num < 0) {
         return "negative"
@@ -9,6 +11,44 @@ const convertToRoman = num => {
     if (num >= 4000) {
         return "too big"
     }
+
+    const numerals = {
+        "I": 1,
+        "V": 5,
+        "X": 10,
+        "L": 50,
+        "C": 100,
+        "D": 500,
+        "M": 1000
+    }
+    
+    let iterableNumerals = Object.entries(numerals)
+    for (let numeralArr of iterableNumerals) {
+        if (numeralArr[1] === num) {
+            return numeralArr[0].toString();
+        }
+    }
+    if (numLength === 1) {
+        if (num > 5) {
+            if (num - 5 === 4) {
+                return "IX"
+            } else {
+                return 'V' + 'I'.repeat(num - 5);
+            }
+        } else {
+            if (num === 4) {
+                return "IV"
+            } else {
+                return "I".repeat(num);
+            }
+        }
+    }
+
+    
+
+    // console.log(num);
+    
 }
+convertToRoman('5');
 
 module.exports = convertToRoman;
--- roman-numeral-converter/convertToRoman.test.js
+++ roman-numeral-converter/convertToRoman.test.js
@@ -4,12 +4,34 @@ describe("Roman Numeral Converter", () => {
   test("it works with empty input", () => {
     expect(convertToRoman("")).toBe("invalid");
   });
-    
-    test("it works with negative input", () => {
-      expect(convertToRoman("-1")).toBe("negative");
-    });
 
-     test("it works with  input >= 4000", () => {
-       expect(convertToRoman("4000")).toBe("too big");
-     });
+  test("it works with negative input", () => {
+    expect(convertToRoman("-1")).toBe("negative");
+  });
+
+  test("it works with  input >= 4000", () => {
+    expect(convertToRoman("4000")).toBe("too big");
+  });
+
+  test("it works with  valid input e.g 5", () => {
+    expect(convertToRoman("5")).toBe("V");
+  });
+
+  test("it works with  valid input e.g 10", () => {
+    expect(convertToRoman("10")).toBe("X");
+  });
+
+  test("it works with  valid input e.g 1000", () => {
+    expect(convertToRoman("1000")).toBe("M");
+  });
+
+  test("it works with  valid input not in set of 7 basic symbols e.g 9", () => {
+    expect(convertToRoman("9")).toBe("IX");
+  });
+  test("it works with  valid input not in set of 7 basic symbols e.g 8", () => {
+    expect(convertToRoman("8")).toBe("VIII");
+  });
+  test("it works with  valid input not in set of 7 basic symbols e.g 2", () => {
+    expect(convertToRoman("2")).toBe("II");
+  });
 });
