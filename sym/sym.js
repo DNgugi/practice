402df0d65827c22d5a2cf650dd249c7d35780b74
DNgugi <dngugi42@gmail.com>
2024-12-20 11:19:03 +0300

    added code to pass test suite

--- sym/sym.js
+++ sym/sym.js
@@ -3,34 +3,34 @@
 // Takes two or more arrays and returns an array of their symmetric difference. The returned array must contain only unique values (no duplicates).
 
 // const sym = (loa) => []; //stub
-const removeDuplicates = (arr) => {
-  if (arr.length === 0) {
-    return arr;
-  }
-  let set = new Set();
-  for (let i = 0; i < arr.length; i++) {
-    if (!set.has(arr[i])) {
-      set.add(arr[i]);
-    }
-  }
-  return Array.from(set);
-};
 
-const symOfTwo = (arr1, arr2) => {
-  return removeDuplicates([
-    ...removeDuplicates(arr1),
-    ...removeDuplicates(arr2),
-  ]);
+const symOfTwo = (a, b) => {
+  if (a.length === 0) {
+    return b;
+  }
+  if (b.length === 0) {
+    return a;
+  }
+  if (a.length === 0 && b.length === 0) {
+    return [];
+  }
+
+  let result = [];
+  a.forEach((item) => {
+    if (b.indexOf(item) === -1 && result.indexOf(item) === -1) {
+      result.push(item);
+    }
+  });
+  b.forEach((item) => {
+    if (a.indexOf(item) === -1 && result.indexOf(item) === -1) {
+      result.push(item);
+    }
+  });
+  return result;
 };
 
 const sym = (...loa) => {
-  let results = [];
+  return loa.reduce(symOfTwo).sort((a, b) => a - b);
 };
-
-console.log(
-  sym([3, 3, 3, 2, 5], [2, 1, 5, 7], [3, 4, 6, 6], [1, 2, 3], [5, 3, 9, 8], [1])
-);
-
-// console.log(sym([1, 2, 3], [5, 2, 1, 4]));
 
 module.exports = sym;
