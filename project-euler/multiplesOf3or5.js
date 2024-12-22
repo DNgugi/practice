00f0e866f0319ceaf1ff933aea292d76ecf731c7
DNgugi <dngugi42@gmail.com>
2024-12-22 10:52:49 +0300

    initial commit

---
+++ project-euler/multiplesOf3or5.js
@@ -0,0 +1,26 @@
+/**
+ * SIGNATURE:
+ * Number -> Number
+ *
+ * PURPOSE:
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+ *
+ * STUB:
+ * const multiplesOf3or5 = (num) => {
+ *  return num;
+ * }
+ *
+ * EXAMPLES:
+ * multiplesOf3Or5(49) -> 543
+ * multiplesOf3Or5(1000) -> 233168
+ * multiplesOf3Or5(8456) -> 16687353
+ * multiplesOf3Or5(19564) -> 89301183
+ *
+ * */
+
+const multiplesOf3or5 = (num) => {
+  return num;
+};
+
+module.exports = multiplesOf3or5;
---
+++ project-euler/multiplesOf3or5.test.js
@@ -0,0 +1,6 @@
+// const chai = require('chai');
+const multiples = require('./multiplesOf3or5');
+
+it("returns return a number", () => {
+    expect(multiples(1)).toBe(1);
+})
