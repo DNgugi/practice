306eb21de33b88fe8b7c737c497d4c5d68bb06e0
DNgugi <dngugi42@gmail.com>
2024-12-29 13:27:52 +0300

    added one passing and one failing test

--- project-euler/largest-palindrome-product/lpp.js
+++ project-euler/largest-palindrome-product/lpp.js
@@ -8,8 +8,22 @@ Find the largest palindrome made from the product of two n-digit numbers.
 
  */
 
-const lpp = (n1, n2) => {
-  return n1 * n2;
+const isPalindrome = require("../../palindrome-checker/isPalindrome");
+
+const lpp = (n) => {
+  const palindromes = [];
+    let max = Number("9".repeat(n));
+    
+  for (let i = max; i > 0; i--) {
+    // console.log("Max", max, "i", i, max * i);
+    if (isPalindrome((max * i).toString())) {
+      palindromes.push(max * i);
+    }
+  }
+  console.log(palindromes);
+  return Math.max(...palindromes);
 };
 
 module.exports = lpp;
+
+console.log(lpp(2));
--- project-euler/largest-palindrome-product/lpp.test.js
+++ project-euler/largest-palindrome-product/lpp.test.js
@@ -1,1 +1,8 @@
-const lpp = require('./lpp');
+const lpp = require("./lpp");
+
+describe("Largest Palindrome Product", () => {
+  it("works with valid inputs", () => {
+    expect(lpp(2)).toBe(9009);
+    expect(lpp(3)).toBe(906609);
+  });
+});
