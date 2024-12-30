82c549bc97f906393ec9065a673a930db1e6e0e6
DNgugi <dngugi42@gmail.com>
2024-12-30 21:03:59 +0300

    added passing code

--- project-euler/largest-palindrome-product/lpp.js
+++ project-euler/largest-palindrome-product/lpp.js
@@ -12,18 +12,18 @@ const isPalindrome = require("../../palindrome-checker/isPalindrome");
 
 const lpp = (n) => {
   const palindromes = [];
-    let max = Number("9".repeat(n));
-    
-  for (let i = max; i > 0; i--) {
-    // console.log("Max", max, "i", i, max * i);
-    if (isPalindrome((max * i).toString())) {
-      palindromes.push(max * i);
+  let max = Number("9".repeat(n));
+
+  for (let j = max; j > 0; j--) {
+    for (let i = max; i > 0; i--) {
+      if (isPalindrome((j * i).toString())) {
+          palindromes.push(j * i);
+      }
     }
   }
-  console.log(palindromes);
   return Math.max(...palindromes);
 };
 
 module.exports = lpp;
 
-console.log(lpp(2));
+console.log(lpp(3));
