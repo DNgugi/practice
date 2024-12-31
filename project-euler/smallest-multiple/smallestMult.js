941cbf74002d3c1bf8136a1c73619230bede5104
DNgugi <dngugi42@gmail.com>
2024-12-31 10:56:00 +0300

    initial commit with failing tests

---
+++ project-euler/smallest-multiple/smallestMult.js
@@ -0,0 +1,30 @@
+/**
+ * Problem 5: Smallest multiple
+ * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+ * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to n?
+ * 
+ * SIGNATURE: 
+ * Number -> Number
+ * 
+ * PURPOSE: 
+ * 
+ * STUB:
+ * const smallestMultiple = (num) => {
+ * return 0;
+ * }
+ *
+ * EXAMPLES:
+ * smallestMult(10) -> 2520
+ * smallestMult(5) -> 60
+ * smallestMult(7) -> 420
+ * smallestMult(10) -> 2520
+ * smallestMult(13) -> 360360
+ * smallestMult(20) -> 232792560.
+ * */
+
+const smallestMult = (num) => {
+  return 0;
+};
+
+
+module.exports = smallestMult;
---
+++ project-euler/smallest-multiple/smallestMult.test.js
@@ -0,0 +1,7 @@
+const smallestMult = require("./smallestMult");
+
+describe("Smallest Multiple", () => {
+  it("works with valid input", () => {
+    expect(smallestMult(5)).toBe(60);
+  });
+});
