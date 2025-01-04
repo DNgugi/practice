811232aaa5ebabd7ffe736fabbb94f6fe99deb6e
DNgugi <dngugi42@gmail.com>
2025-01-04 22:29:23 +0800

    initial commit

---
+++ project-euler/sum-square-diff/sumSquareDiff.js
@@ -0,0 +1,7 @@
+const sumSquareDiff = (n) => {
+  return n;
+};
+
+sumSquareDiff(5);
+
+module.exports = sumSquareDiff;
---
+++ project-euler/sum-square-diff/sumSquareDiff.test.js
@@ -0,0 +1,7 @@
+const sumSquareDiff = require("./sumSquareDiff");
+
+describe("Sum Square Difference", () => {
+  it("works with valid input", () => {
+    expect(sumSquareDiff(5)).toBe(60);
+  });
+});
